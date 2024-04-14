028dc90e6d573cf6adf0c589dcc62ae9e3d05c9a
Hudson <hudson@ziroau.com>
2024-04-15 02:18:22 +1000

    Init repo

---
+++ Makefile
@@ -0,0 +1,5 @@
+TAG = hudsonm62/short-sha
+EXAMPLE_SHA = I2E456789OI2E456789OI2E456789OI2E456789O
+
+build:
+	docker build -t $(TAG) .
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -l
 
-# Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
-
-# Use workflow commands to do things like set debug messages
-echo "::notice file=entrypoint.sh,line=7::$GREETING"
+SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-7)
 
 # Write outputs to the $GITHUB_OUTPUT file
-echo "time=$(date)" >>"$GITHUB_OUTPUT"
+echo "sha=${SHORT_SHA}" >> "$GITHUB_OUTPUT"
 
 exit 0
